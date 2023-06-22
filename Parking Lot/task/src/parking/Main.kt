008980c5944aca5cbd895ac15cfa90c4b8266f81
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-06-22 15:49:14 +0100

    stage 4 complete

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -3,24 +3,34 @@ package parking
 data class Car(val reg:String, val color: String)
 
 fun main() {
-    val parking = MutableList<Car?>(20) {null}
+    var parking: MutableList<Car?>? = null
     do {
         val args = readln().split(" ")
         val command = args[0]
-        if (command == "park") {
-            val car = Car(args[1], args[2])
-            val parkBay = parking.park(car)
-            println(
+        println( when {
+            command == "exit" -> ""
+            command == "create" -> {
+                val slots = args[1].toInt()
+                parking = MutableList(slots) { null }
+                "Created a parking lot with $slots spots."
+            }
+            parking == null -> "Sorry, a parking lot has not been created."
+            command == "park" -> {
+                val car = Car(args[1], args[2])
+                val parkBay = parking.park(car)
                 if (parkBay != -1)
                     "${car.color} car parked in spot $parkBay."
-                else "Sorry, the parking lot is full.")
-        } else if (command == "leave") {
-            val spotEmpty = parking.leave(args[1].toInt())
-            println(
+                else "Sorry, the parking lot is full."
+            }
+            command == "leave" -> {
+                val spotEmpty = parking.leave(args[1].toInt())
                 if (spotEmpty != -1)
                     "Spot $spotEmpty is free."
-                else "There is no car in spot ${args[1]}.")
-        }
+                else "There is no car in spot ${args[1]}."
+            }
+            command == "status" -> parking.status()
+            else -> ""
+        })
     } while (command != "exit")
 }
 
@@ -40,4 +50,11 @@ fun MutableList<Car?>.leave(spot: Int): Int {
         return spot
     }
     return -1
+}
+
+fun MutableList<Car?>.status(): String {
+    if (filterNotNull().isEmpty()) return "Parking lot is empty."
+    val lot = mapIndexed { index, car -> if (car != null) "${index + 1} ${car.reg} ${car.color}" else null}
+        .filterNotNull()
+    return lot.joinToString("\n")
 }
