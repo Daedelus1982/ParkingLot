5df6094151e2efbe33ce5c3d938c61b2fc2a027d
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-06-23 14:59:31 +0100

    stage 5 beforeb delete main

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -3,7 +3,16 @@ package parking
 data class Car(val reg:String, val color: String)
 
 fun main() {
-    var parking: MutableList<Car?>? = null
+    val lot = MutableList<Car?>(4) { null }
+    lot.park(Car("KA-01-HH-9999", "White"))
+    lot.park(Car("KA-01-HH-3672", "White"))
+    lot.park(Car("Rs-P-N-21", "Red"))
+    lot.park(Car("Rs-P-N-22", "Red"))
+    main2(lot)
+}
+fun main2(lot: MutableList<Car?>) {
+    //var parking: MutableList<Car?>? = null
+    var parking = lot
     do {
         val args = readln().split(" ")
         val command = args[0]
@@ -29,6 +38,9 @@ fun main() {
                 else "There is no car in spot ${args[1]}."
             }
             command == "status" -> parking.status()
+            command == "reg_by_color" -> parking.regByColour(args[1])
+            command == "spot_by_color" -> parking.spotByColor(args[1])
+            command == "spot_by_reg" -> parking.spotByReg(args[1])
             else -> ""
         })
     } while (command != "exit")
@@ -57,4 +69,27 @@ fun MutableList<Car?>.status(): String {
     val lot = mapIndexed { index, car -> if (car != null) "${index + 1} ${car.reg} ${car.color}" else null}
         .filterNotNull()
     return lot.joinToString("\n")
+}
+
+fun MutableList<Car?>.regByColour(color: String): String {
+    val regList = filterNotNull()
+        .filter { it.color.equals(color, true) }
+        .map { it.reg }
+
+    return if (regList.isEmpty()) "No cars with color $color were found." else regList.joinToString(", ")
+}
+
+fun MutableList<Car?>.spotByColor(color: String): String {
+    val spotList = mapIndexed { index, car ->  if (car?.color.equals(color, true)) index + 1 else -1 }
+        .filter { it != -1 }
+
+    return if (spotList.isEmpty()) "No cars with color $color were found." else spotList.joinToString(", ")
+}
+
+fun MutableList<Car?>.spotByReg(reg: String): String {
+    val spot = mapIndexed { index, car ->  if (car?.reg.equals(reg, true)) index + 1 else -1 }
+        .filter { it != -1 }
+        .firstOrNull()
+
+    return if (spot == null) "No cars with registration number $reg were found." else "$spot"
 }
